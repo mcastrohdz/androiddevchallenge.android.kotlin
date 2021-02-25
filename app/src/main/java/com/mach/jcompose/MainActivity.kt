330c95a23063ed7b49dcd86e93152a94cef35818
Marco Castro <marco.castro@rappi.com>
2021-02-25 00:28:41 -0600

    Simple image with bottom margin

--- app/src/main/java/com/mach/jcompose/MainActivity.kt
+++ app/src/main/java/com/mach/jcompose/MainActivity.kt
@@ -3,26 +3,47 @@ package com.mach.jcompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Hey")
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text (text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.height(16.dp))
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun PreviewGreeting() {
-    Greeting("Marco")
+    NewsStory()
 }
